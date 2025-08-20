98c2bfc653b1c75be267e2fd5fe89c59cf19b785
Kenramiscal1106 <code.ramiscalken@gmail.com>
2025-08-20 10:35:11 +0800

    add navigation tab

--- sidebar.js
+++ sidebar.js
@@ -1,12 +1,39 @@
 import { getCategories, getCategoryTodos } from "./index.js";
 
+const homeTab = document.querySelector(".sidebar__home-tab");
+homeTab.classList.add("tab--active");
+homeTab.addEventListener("click", handleHome);
+
 export function renderCategoryTab(category) {
-  const categoryTabs = document.querySelector("#category-tabs");
-  const listElement = document.createElement("button");
-  listElement.textContent = category.categoryIcon + " " + category.name;
-  categoryTabs.appendChild(listElement);
-  listElement.addEventListener("click", async () => {
-    console.log(await getCategoryTodos(category.id))
-  });
-  
+  const tabContainer = document.querySelector("#category-tabs");
+  const categoryTab = document.createElement("button");
+  const iconSpan = document.createElement("span");
+  iconSpan.textContent = category.categoryIcon;
+  const nameSpan = document.createElement("span");
+  nameSpan.textContent = category.name;
+  categoryTab.appendChild(iconSpan);
+  categoryTab.appendChild(nameSpan);
+  categoryTab.setAttribute("data-category-id", category.id);
+  categoryTab.classList.add("category");
+  tabContainer.appendChild(categoryTab);
+  categoryTab.addEventListener("click", handleCategoryChange(category));
+}
+
+function handleCategoryChange(category) {
+  return async (event) => {
+    const activeTab = document.querySelector("button.tab--active");
+    event.target.classList.add("tab--active");
+    console.log(await getCategoryTodos(category.id));
+
+    if (!activeTab) return;
+    if (activeTab.dataset.categoryId === category.id) return;
+
+    activeTab.classList.remove("tab--active");
+  };
+}
+
+async function handleHome(event) {
+  const activeTab = document.querySelector("button.tab--active");
+  event.target.classList.add("tab--active");
+  activeTab.classList.remove("tab--active");
 }
