a6ad36c911b4d75e300c005e0d169bd954179039
Marc Tristan Victoria <marctristanvictoria@gmail.com>
2025-08-24 22:30:49 +0800

    removed pre-existing template, added renderKanbanItem function

--- kanban.js
+++ kanban.js
@@ -1,4 +1,48 @@
 // Kanban Drag and Drop
+import { getKanbanTodos } from "./index.js";
+
+/**
+ * @param {{
+ * id: string,
+ * taskName: string,
+ * deadline: string,
+ * categoryId: string,
+ * status: "pending" | "ongoing" | "done"
+ * }} item
+ */
+export function renderKanbanItem(item) {
+  const defaultContainer = document.querySelector(".mode-container--kanban");
+
+  const item = document.createElement("div");
+  const timeLeft = document.createElement("div");
+
+  item.classList.add("item");
+  timeLeft.classList.add("time_left");
+
+  timeLeft.innerHTML = `
+    <svg
+    width="18"
+    height="19"
+    viewBox="0 0 18 19"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M9 17C13.1421 17 16.5 13.6422 16.5 9.50005C16.5 5.35791 13.1421 2.00005 9 2.00005C4.85786 2.00005 1.5 5.35791 1.5 9.50005C1.5 13.6422 4.85786 17 9 17Z"
+      stroke="#E5715C"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M9 5V9.5L12 11"
+      stroke="#E5715C"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>`;
+}
 
 const lists = document.querySelectorAll(".list");
 const statuses = document.querySelectorAll(".status");
